4406e04b72ef9b2665592c2d3b84e532c6970436
user.name : "Kunal-Sarpal <user.email: "sarpalkunal7@gmail.com>
2024-07-14 12:51:39 +0530

    commit

--- Shhashcode/afterbinary/cyclesort.java
+++ Shhashcode/afterbinary/cyclesort.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+
 public class cyclesort {
+    static void swap(int arr[],int i, int j){
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
     public static void main(String[] args) {
         
+
+        int arr[] = {5,4,3,2,1};
+        int index = 0;
+        while(index < arr.length){
+            int el = arr[index];
+            int ai = el - 1;
+
+            if(arr[index] < arr.length && arr[index] != arr[ai] ){
+                swap(arr,index,ai);
+            }
+            else{
+                index++;
+
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
